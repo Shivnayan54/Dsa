8bf8f452a89cd3f2b52d30b5560a2d2e599e97fa
shivnayan <nayan54451@gmail.com>
2026-07-16 19:45:09 +0530

    initial DSa commit

--- index.cpp
+++ index.cpp
@@ -1,12 +1,45 @@
-#include<iostream> 
+#include<iostream>
 using namespace std; 
 
-void sayHello() { 
-    cout << "Hello, World!" << endl; 
-}
 
-int main() {
-    sayHello(); 
-    return 0;
-}
+int lastOccurrence(int arr[], int n, int target) 
+{
+    int left = 0; 
+    int right = n-1 ; 
 
+    int ans = -1;
+
+    while(left <= right)
+    {
+
+        int mid = (left + right) /2 ; 
+
+    if(arr[mid] == target)
+    {
+        ans = mid; 
+
+        left = mid+1; 
+    }
+
+    else if(target > arr[mid])
+    {
+        left = mid+1; 
+    }
+
+    else{
+        right = mid-1; 
+    }
+
+    return ans; 
+}
+}
+int main()
+{
+    int arr[] = {10,20,20,20,30,40,50}; 
+
+    int n = 7; 
+
+    int target = 20; 
+ss
+    cout << lastOccurrence(arr, n, target);
+}
