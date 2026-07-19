1aa626cb4b2fc0cdade5e50039209fea681801e3
shivnayan <nayan54451@gmail.com>
2026-07-19 11:50:16 +0530

    Find the Second Largest in array

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -4,23 +4,28 @@ using namespace std;
 int main() 
 {
 
-    int arr[5] = {5, 2 , 9, 1, 7};
+    int arr[5] = {10 , 40 ,20 ,50 ,30};
 
     int largest = arr[0];
-
+    int secondLargest = 0; 
     for(int i =1 ; i<5 ; i++){ 
 
-        if( largest > arr[i]){
+        if( arr[i] > largest ){
             
-            largest = largest; 
-
-        } else {
+            secondLargest = largest; 
             largest = arr[i]; 
+            
+        }  else if (arr[i] > secondLargest && arr[i] != largest)
+        {
+            secondLargest = arr[i];
         }
 
     }
 
-    cout<<largest ;
+    cout<<"Largest is : " <<largest<<" " << endl ; 
+
+    cout<<"The secondLargest is: "<<secondLargest; 
+    
 
     return 0;
 }
