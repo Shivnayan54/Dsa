5f1b6eb40a9f7285511a96da93e6e67c5c531584
shivnayan <nayan54451@gmail.com>
2026-07-19 16:41:57 +0530

    Count occurrences

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 int main(){ 
 
-    int arr[5] = {10 , 20 , 30 , 40 , 50}; 
+    int arr[5] = {10 , 20 , 20 , 30 , 20}; 
 
-    int target = 30 ;
+    int target = 20 ;
+    int occurrences = 0;
 
     for(int i=0; i<5 ; i++){
 
         if(arr[i] == target){
 
-            cout<<"Target found at index: "<<i<<endl;
+            occurrences++;
 
         }
 
+        
     }
 
+    cout<<"Number of occurrences of " << target << " is: " << occurrences << endl;
     return 0; 
 }
