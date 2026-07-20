50bb6b8ac4c4248dd15d2e2e91fac24c69fa39d3
shivnayan <nayan54451@gmail.com>
2026-07-20 10:48:14 +0530

    Find the maximum element

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -1,23 +1,21 @@
 #include<iostream>
-using namespace std; 
+using namespace std;
 
-int main(){ 
+int main() {
 
-    int arr[5] = {10 , 20 , 20 , 30 , 20}; 
+    int arr[5] = {10, 20, 20, 30, 20};
 
-    int mini = arr[0];
+    int maxi = arr[0];
 
-    for(int i=1; i<5 ; i++) {
-        
-        if(arr[i] < mini) {
-            mini = arr[i];
+    for (int i = 1; i < 5; i++) {
+
+        if (arr[i] > maxi) {
+            maxi = arr[i];
         }
+
     }
 
-        
-    }
-    
-    cout<<"The minimum element in the array is : "<<mini<<endl;
-    
-    return 0; 
+    cout << "The maximum element in the array is : " << maxi << endl;
+
+    return 0;
 }
