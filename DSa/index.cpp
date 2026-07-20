7024e2883e895756e544c81d202f2cf446f2e5f2
shivnayan <nayan54451@gmail.com>
2026-07-20 10:40:53 +0530

    Find the minimum element

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -5,20 +5,19 @@ int main(){
 
     int arr[5] = {10 , 20 , 20 , 30 , 20}; 
 
-    int target = 20 ;
-    int occurrences = 0;
+    int mini = arr[0];
 
-    for(int i=0; i<5 ; i++){
-
-        if(arr[i] == target){
-
-            occurrences++;
-
+    for(int i=1; i<5 ; i++) {
+        
+        if(arr[i] < mini) {
+            mini = arr[i];
         }
+    }
 
         
     }
-
-    cout<<"Number of occurrences of " << target << " is: " << occurrences << endl;
+    
+    cout<<"The minimum element in the array is : "<<mini<<endl;
+    
     return 0; 
 }
