69b1384a1ce33f67efa8ea96495d19e88179db31
shivnayan <nayan54451@gmail.com>
2026-07-21 19:50:41 +0530

    Reverse only a part of an array in easy way

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -1,28 +1,19 @@
-#include<iostream>
+#include<iostream> 
 using namespace std; 
 
-int main() 
-{
+int main() { 
+    
+    int arr[7] = {1 , 2 , 3 , 4 , 5 , 6 , 7} ; 
 
-    int arr[7] = {1, 0, 2, 0, 3, 0, 4};
+    int n =7; 
 
-    int n = 7; 
+    for(int i =6; i>=2 ; i--){ 
 
-    int index = 0; 
+        cout<<i ; 
 
-    for(int i=0; i<n ; i++) { 
+    }    
 
-        if(arr[i] != 0 ) {
-          swap(arr[i] , arr[index]) ; 
-          index++ ; 
+     return 0; 
 
-        }
 
-    }
-
-    for(int i=0 ; i<n ; i++){ 
-        cout<<arr[i] ; 
-    }
-
-    return 0; 
-}
+}   
