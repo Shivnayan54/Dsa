e2088f9c5698543b0306606b5d3cbe7767da696a
shivnayan <nayan54451@gmail.com>
2026-07-23 08:59:43 +0530

    initial commit

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -5,10 +5,10 @@ int main() {
  
 int n = 6; 
 
-int arr[n] = {1, 2, 3,  6, 5, 4};
+int arr[n] = {1,  3,  6, 5, 4};
 
 
-int target = 4; 
+int target = 2; 
 
 int left = 0;
 int right = n - 1;
@@ -17,17 +17,20 @@ while(left <= right) {
 
     int mid = (left + right) / 2; 
 
-    if(arr[mid] == target) {
-        cout << "Element found at index: " << mid << endl;
+    if(arr[mid] == target){
+        cout<<"element found at index: " <<mid ; 
         return 0; 
-    } else if(target > arr[mid]) {
-        left = mid + 1; 
-    } else {
-        right = mid - 1; 
+    }
+    else if(target > arr[mid]){
+        left = mid+1; 
+
+    }else {
+        right = mid -1 ; 
     }
 
-}
+}   
 
+cout<< "Element Not found"; 
 return 0; 
 
-}
+}  
