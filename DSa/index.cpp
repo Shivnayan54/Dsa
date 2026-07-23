d42a4c389482ec650d284e30444595d8971d024f
shivnayan <nayan54451@gmail.com>
2026-07-23 15:51:39 +0530

    Search Last Occurence

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -1,36 +1,41 @@
 #include<iostream>
-using namespace std; 
+using namespace std;
 
-int main() { 
+int main() {
 
-    int arr[] = {1, 2, 2, 2, 3, 4, 5}; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
+    int arr[] = {1, 2, 2, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int target = 2; 
+    int target = 2;
 
-    int left = 0; 
-    int right = n-1; 
+    int left = 0;
+    int right = n - 1;
 
-    int ans = -1; 
+    int ans = -1;
 
-    while (left <= right){ 
+    while(left <= right) {
 
-        int mid = left + (right - left ) / 2; 
+        int mid = left + (right - left) / 2;
 
-        if(arr[mid] == target){ 
-            ans = mid; 
-            right = mid-1; 
+        if(arr[mid] == target) {
+            ans = mid;
+            left = mid + 1;      // Search on the right
         }
-        else if(arr[mid] < target){ 
-            left = mid+1; 
-        }else {
-            right = mid- 1; 
+
+        else if(arr[mid] < target) {
+            left = mid + 1;
         }
+
+        else {
+            right = mid - 1;
+        }
+
     }
-    if(ans != -1) 
-    cout<<"First Occurrence is at index: " << ans; 
-    else
-    cout<<"Element not found"; 
 
-    return 0; 
+    if(ans != -1)
+        cout << "Last Occurrence is at index: " << ans;
+    else
+        cout << "Element not found";
+
+    return 0;
 }
