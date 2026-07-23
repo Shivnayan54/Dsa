7741945797029cf87f418d90c57dec4b24cef6e4
shivnayan <nayan54451@gmail.com>
2026-07-23 15:21:43 +0530

    Search Insert Position

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -1,38 +1,26 @@
 #include<iostream>
-using namespace std;
+using namespace std; 
 
-int main() {
+int main() 
+{
 
-    int arr[] = {1, 3, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[10] = {1 , 3, 4 , 5}; 
+    
+    int n = 4; 
 
-    int target;
-    cout << "Enter the target element: ";
-    cin >> target;
+    int target = 2; 
+    int pos = 1; 
 
-    int left = 0;
-    int right = n - 1;
-
-    while(left <= right) {
-
-        int mid = left + (right - left) / 2;
-
-        if(arr[mid] == target) {
-            cout << "Target found at index: " << mid << endl;
-            return 0;
-        }
-
-        else if(target > arr[mid]) {
-            left = mid + 1;
-        }
-
-        else {
-            right = mid - 1;
-        }
+    for(int i = n; i>pos ; i--)
+    {
+       arr[i] = arr[i-1]; 
     }
+    
+    arr[pos] = target;
+    n++; 
 
-    // If target is not found
-    cout << "Insert Position: " << left << endl;
-
-    return 0;
+    for(int i=0; i<n ;i++){ 
+        cout<< arr[i]<<" "; 
+    }
+    return 0; 
 }
