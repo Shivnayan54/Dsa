2cebbf1c6bf90bf191e55b00b6aa8b8a7a564d22
shivnayan <nayan54451@gmail.com>
2026-07-24 09:38:56 +0530

    Sum of two numbers using function

--- DSa/index.cpp
+++ DSa/index.cpp
@@ -1,72 +1,15 @@
 #include<iostream>
-using namespace std;
+using namespace std; 
 
-int firstOccurrence(int arr[], int n, int target) {
+int sum( int a , int b) {
 
-    int left = 0;
-    int right = n - 1;
-    int ans = -1;
-
-    while(left <= right) {
-
-        int mid = left + (right - left) / 2;
-
-        if(arr[mid] == target) {
-            ans = mid;
-            right = mid - 1;
-        }
-        else if(arr[mid] < target) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
-    }
-
-    return ans;
+   return a+b; 
 }
 
-int lastOccurrence(int arr[], int n, int target) {
+int main(){
 
-    int left = 0;
-    int right = n - 1;
-    int ans = -1;
+    int x = sum(10 , 20); 
+    cout<< x; 
 
-    while(left <= right) {
-
-        int mid = left + (right - left) / 2;
-
-        if(arr[mid] == target) {
-            ans = mid;
-            left = mid + 1;
-        }
-        else if(arr[mid] < target) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
-    }
-
-    return ans;
-}
-
-int main() {
-
-    int arr[] = {1, 2, 2, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    int target = 2;
-
-    int first = firstOccurrence(arr, n, target);
-    int last = lastOccurrence(arr, n, target);
-
-    if(first == -1) {
-        cout << "Count = 0";
-    }
-    else {
-        cout << "Count = " << last - first + 1;
-    }
-
-    return 0;
+   
 }
