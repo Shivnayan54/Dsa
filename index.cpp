bdf46ba334dc478591f8a9d87e70c5e9795ce2da
shivnayan <nayan54451@gmail.com>
2026-07-26 22:47:34 +0530

    First Occurrence, Last Occurrence , Count Occurrences , LowerBound , Upper Bound

--- index.cpp
+++ index.cpp
@@ -1,29 +1,134 @@
 #include<iostream>
-using namespace std; 
+using namespace std;
 
-int main() { 
+// First Occurrence
+int firstOccurrence(int arr[], int n, int target)
+{
+    int left = 0;
+    int right = n - 1;
+    int ans = -1;
 
-    int n=7; 
-    int arr[n] = {10 ,20 , 20 , 20 ,30 , 40 , 50}; 
-    int target = 40; 
-    int left = 0; 
-    int right = n-1; 
-    int ans = 0; 
-    int firstOccurrence = 0; 
-    
     while(left <= right)
     {
-        int mid = left + (right - left) / 2; 
+        int mid = left + (right - left) / 2;
 
-        if(arr[mid] == target && arr[mid] >= target)
+        if(arr[mid] == target)
         {
-            ans = mid; 
-            right = mid-1;
-        }else{ 
-            left = mid+1; 
+            ans = mid;
+            right = mid - 1;     // Search on left
+        }
+        else if(arr[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
         }
     }
 
-    cout<<ans ; 
-    return 0; 
+    return ans;
+}
+
+// Last Occurrence
+int lastOccurrence(int arr[], int n, int target)
+{
+    int left = 0;
+    int right = n - 1;
+    int ans = -1;
+
+    while(left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if(arr[mid] == target)
+        {
+            ans = mid;
+            left = mid + 1;      // Search on right
+        }
+        else if(arr[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+// Lower Bound
+int lowerBound(int arr[], int n, int target)
+{
+    int left = 0;
+    int right = n - 1;
+    int ans = n;
+
+    while(left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if(arr[mid] >= target)
+        {
+            ans = mid;
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+// Upper Bound
+int upperBound(int arr[], int n, int target)
+{
+    int left = 0;
+    int right = n - 1;
+    int ans = n;
+
+    while(left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if(arr[mid] > target)
+        {
+            ans = mid;
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int arr[] = {10, 20, 20, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int target = 20;
+
+    int first = firstOccurrence(arr, n, target);
+    int last = lastOccurrence(arr, n, target);
+
+    cout << "First Occurrence : " << first << endl;
+    cout << "Last Occurrence  : " << last << endl;
+
+    if(first != -1)
+        cout << "Count Occurrence : " << last - first + 1 << endl;
+    else
+        cout << "Count Occurrence : 0" << endl;
+
+    cout << "Lower Bound      : " << lowerBound(arr, n, target) << endl;
+    cout << "Upper Bound      : " << upperBound(arr, n, target) << endl;
+
+    return 0;
 }
