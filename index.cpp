4527a46e4da69e968a37499fa38d79d78c138d31
shivnayan <nayan54451@gmail.com>
2026-07-26 22:42:30 +0530

    First occurrences and Lowerbound

--- index.cpp
+++ index.cpp
@@ -3,18 +3,19 @@ using namespace std;
 
 int main() { 
 
-    int n=5; 
-    int arr[n] = {10 ,20 ,30 , 40 , 50}; 
+    int n=7; 
+    int arr[n] = {10 ,20 , 20 , 20 ,30 , 40 , 50}; 
     int target = 40; 
     int left = 0; 
     int right = n-1; 
     int ans = 0; 
+    int firstOccurrence = 0; 
     
     while(left <= right)
     {
         int mid = left + (right - left) / 2; 
 
-        if(arr[mid] > target)
+        if(arr[mid] == target && arr[mid] >= target)
         {
             ans = mid; 
             right = mid-1;
